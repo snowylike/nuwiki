2ce087add9805641c93021f562b017685bedc5bd
Hendrik Kosmol <hkosmol@gmail.com>
2018-10-03 16:51:39 +0200

    generating first styling data

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,4 +6,14 @@ function loader($class) {
     require_once('src'.DIRECTORY_SEPARATOR.$class.'.php');
 }
 
+function prepper() {
+    $data = array(
+        'title' => 'NuWiki',
+        'charset' => 'utf-8',
+        'author' => 'Hendrik Kosmol',
+        'style' => 'style',
+        'description' => 'A basic wiki made in php');
+    file_put_contents('data/site.txt', serialize($data));
+}
+
 spl_autoload_register('loader');
--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require('config'.DIRECTORY_SEPARATOR.'bootstrap.php');
 
 define('nesting', 'controllers');
+if(!file_exists('data/site.txt')) {
+    prepper();
+}
 
 new controllers\SiteController();
 
