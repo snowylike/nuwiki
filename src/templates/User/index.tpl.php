51c26e105ee648c8597058da375a6b624711f360
Hendrik Kosmol <hkosmol@gmail.com>
2018-10-04 13:21:42 +0200

    lists used to navigate user controls

--- src/templates/User/index.tpl.php
+++ src/templates/User/index.tpl.php
@@ -6,6 +6,8 @@
             <li><?= $e->getName(); ?></li>
             <li><?= $e->getSurname(); ?></li>
             <li><?= $e->getNick(); ?></li>
+            <li><a href="index.php?controller=User&action=del&id=<?= $e->getId(); ?>">löschen</a></li>
+            <li><a href="index.php?controller=User&action=mod&id=<?= $e->getId(); ?>">modifizieren</a></li>
         </ul>
     </li>
 <?php endforeach; ?>
